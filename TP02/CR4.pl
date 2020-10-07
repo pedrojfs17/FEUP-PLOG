a43e643fbc470676d22e0aec702bd0f9e61c0969
pedrojfs17 <up201806227@fe.up.pt>
2020-10-07 11:58:26 +0100

    Started tp02

---
+++ TP02/CR4.pl
@@ -0,0 +1,19 @@
+% a)
+
+fatorial(0, 1).
+fatorial(1, 1).
+fatorial(N, F) :- 
+    N > 1,
+    N1 is N-1, fatorial(N1, F1),
+    F is N*F1.
+
+
+% b)
+
+fibonacci(0, 1).
+fibonacci(1, 1).
+fibonacci(N, F) :-
+    N > 1,
+    N1 is N-1, fibonacci(N1, F1),
+    N2 is N-2, fibonacci(N2, F2),
+    F is F1+F2.
