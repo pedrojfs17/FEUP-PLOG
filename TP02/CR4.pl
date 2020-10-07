5ab267a7f65b9a97c6744ef8f4a02c8514948016
pedrojfs17 <up201806227@fe.up.pt>
2020-10-07 12:21:40 +0100

    Done factorial

--- TP02/CR4.pl
+++ TP02/CR4.pl
@@ -1,11 +1,21 @@
 % a)
 
 fatorial(0, 1).
-fatorial(1, 1).
 fatorial(N, F) :- 
-    N > 1,
+    N > 0,
     N1 is N-1, fatorial(N1, F1),
     F is N*F1.
+
+
+% a) (melhor solução)
+
+fact(N, F) :- fact(N, 1, F).
+fact(0, F, F).
+fact(N, Acc, F) :-
+    N > 0,
+    N1 is N-1,
+    Acc1 is Acc * N,
+    fact(N1, Acc1, F).
 
 
 % b)
