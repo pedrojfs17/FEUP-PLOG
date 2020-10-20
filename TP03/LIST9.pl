cae3e535f4728701179fa87b7c6ca05e25a36738
pedrojfs17 <up201806227@fe.up.pt>
2020-10-20 18:51:28 +0100

    Finished 9

--- TP03/LIST9.pl
+++ TP03/LIST9.pl
@@ -11,3 +11,17 @@ substitui(X, Y, Lista1, Lista2) :-
     X \= H,
     append([H], L2, Lista2),
     substitui(X, Y, LT, L2).
+
+
+elimina_duplicados([],[]).
+
+elimina_duplicados(Lista1, Lista2) :-
+    Lista1 = [H|T],
+    \+member(H,T),
+    append([H], L2, Lista2),
+    elimina_duplicados(T, L2).
+
+elimina_duplicados(Lista1, Lista2) :-
+    Lista1 = [H|T],
+    member(H,T),
+    elimina_duplicados(T, Lista2).
