7954dd90ff68bc700a82ec5f40d18ae895618d1b
Pedro Seixas <49153615+pedrojfs17@users.noreply.github.com>
2020-11-25 12:15:02 +0000

    Fixed exercise

--- TP08/IPLR7.pl
+++ TP08/IPLR7.pl
@@ -3,7 +3,4 @@
 precoPeru(Preco) :-
     A in 1..9,
     B in 0..9,
-    Total #= A*1000 + 670 + B,
-    Resto #= Total mod 72,
-    Resto #= 0,
-    Preco is (1000 * A + 670 + B) / 72.
+    Preco * 72 #= A * 1000 + 670 + B.
